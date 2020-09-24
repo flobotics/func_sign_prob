cd3a4648450419f5bfc63f59d0eb289fbf282ef4
inflo <info@flobotics-robotics.com>
2020-09-25 01:37:39 +0200

    added ret-type long long __attribute__ ((vector_size(2))) as delete

--- ubuntu-20-04-scripts/build-tf-embedding-dataset/tokenize_att_disassembly_from_pickle.py
+++ ubuntu-20-04-scripts/build-tf-embedding-dataset/tokenize_att_disassembly_from_pickle.py
@@ -120,6 +120,8 @@ def get_raw_return_type_from_gdb_ptype(gdb_ptype):
             return 'delete'
         elif 'GLcontext' in raw_gdb_ptype:
             return 'delete'
+        elif raw_gdb_ptype == 'long long __attribute__ ((vector_size(2)))':
+            return 'delete'
         
         ### check if we directly find a valid return type
         for return_type in return_type_list:
