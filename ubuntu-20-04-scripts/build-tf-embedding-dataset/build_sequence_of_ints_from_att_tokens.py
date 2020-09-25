1f9bf67b144cedd6659e942fd533620b24b00d74
inflo <info@flobotics-robotics.com>
2020-09-26 00:30:46 +0200

    debug print

--- ubuntu-20-04-scripts/build-tf-embedding-dataset/build_sequence_of_ints_from_att_tokens.py
+++ ubuntu-20-04-scripts/build-tf-embedding-dataset/build_sequence_of_ints_from_att_tokens.py
@@ -147,6 +147,7 @@ def save_embs_to_pickle(embeddings_part, embedding_build_dir, embedding_build_fi
     ### check size
     size_in_bytes = sys.getsizeof(ds_tmp_bucket)
     print(f'size-in-bytes: {size_in_bytes}')
+    print(f'len : {len(ds_tmp_bucket)}')
     if size_in_bytes > 30000000:
         #print(f'save pickle')
         ### save as pickle
