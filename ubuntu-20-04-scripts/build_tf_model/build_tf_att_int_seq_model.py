fe3d9ba40fb7cfa2a484c4dfaf570572b362d090
ubu <ubu@ubu-VirtualBox>
2020-09-27 00:10:20 +0200

    extended, but doesnt work

--- ubuntu-20-04-scripts/build_tf_model/build_tf_att_int_seq_model.py
+++ ubuntu-20-04-scripts/build_tf_model/build_tf_att_int_seq_model.py
@@ -6,18 +6,32 @@ import numpy as np
 import os
 from datetime import datetime
 
-
-
-def build_tf_dataset(path_to_int_seq_pickle):
-    ### read full_dataset_att_int_seq.pickle
-    pickle_file = open(path_to_int_seq_pickle, 'rb')
+def get_pickle_file_content(full_path_pickle_file):
+    pickle_file = open(full_path_pickle_file,'rb')
     pickle_list = pickle.load(pickle_file, encoding='latin1')
     pickle_file.close()
     
-    ### 
+    return pickle_list
+
+
+def get_ret_type_dict(pickle_list):
+    ret_type_set = set()
     
-    label_as_int = vocab_ret_types[label]
-    label_as_one_hot = tf.one_hot(label_as_int, len(vocab_ret_types))
+    for content in pickle_list:
+        for int_seq, ret_type in content:
+            ret_type_set.add(ret_type)
+            
+            
+    ### build ret_type dict
+    ret_type_dict = {v:k for v,k in enumerate(ret_type_set, start=1)}
+            
+    return ret_type_dict
+            
+
+    
+    
+
+def add_one_item_to_tf_dataset(func_as_int_list, label_as_one_hot):
     
     if dataset_counter == 0:
         #dataset = tf.data.Dataset.from_tensors( ( func_as_int_list,label_as_int ))
@@ -34,6 +48,7 @@ def build_tf_dataset(path_to_int_seq_pickle):
     component = next(iter(dataset))
     print(component)
     
+    return dataset
     
     
 def split_tf_dataset():
@@ -77,14 +92,41 @@ def shuffle_and_pad():
 def main():
     path_to_int_seq_pickle = "../../ubuntu-20-04-datasets/full_dataset_att_int_seq.pickle"
     
-    ### build tf dataset from pickle file
-    tf_ds = build_tf_dataset(path_to_int_seq_pickle)
+    ###read out full ds pickle
+    pickle_file_content = get_pickle_file_content(path_to_int_seq_pickle)
     
+    ### get return type dict
+    ret_type_dict = get_ret_type_dict(path_to_int_seq_pickle)
     
+    ### for content
+    for content in pickle_file_content:
+        ### for every item in list
+        for func_as_int_list, label in content: 
+            ### build tf-one-hot
+            label_as_int = ret_type_dict[label]
+            label_as_one_hot = tf.one_hot(label_as_int, len(ret_type_dict))
+            
+            ### build tf dataset
+            full_tf_ds = add_one_item_to_tf_dataset(func_as_int_list, label_as_one_hot)
+
+    #####################
+    ### Now we got our tf dataset which we can easily push into model.compile
     
+    ### Next we create our model
+    #####################
+
     embedding_dim=64
     
-    print(len(vocab))
+    ### get number of "words" in our vocabulay of int_seq (NOT ret-types)
+    ### open vocab file
+    vocab_file = open("../../ubuntu-20-04-datasets/full_dataset_att_int_seq_vocabulary.pickle")
+    vocab = pickle.load(vocab_file, encoding='latin1')
+    vocab_file.close()
+    print(f'Number of items in vocabulary: {len(vocab)}')
+    
+    #####################
+    ### build tf model
+    #####################
     
     #model = keras.Sequential([
     #  layers.Embedding(len(vocab), embedding_dim),
@@ -93,57 +135,7 @@ def main():
     #  layers.Dense(len(vocab_ret_types))
     #])
     
-    model = keras.Sequential([
-        layers.Embedding(len(vocab)+1, embedding_dim, mask_zero=True),
-        layers.GlobalAveragePooling1D(),
-        layers.Dense(32, activation='relu'),
-        layers.Dense(len(vocab_ret_types))
-    ])
-        
-        
-    
-    
-    
-    #model.compile(optimizer='adam',
-    #              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-    #              metrics=['accuracy'])
-        
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    
-    
-    date_str = datetime.now().strftime("%Y%m%d-%H%M%S")
-    
-    #checkpoint_filepath = 'logs/' + date_str
-    checkpoint_filepath = 'logs/checkpoint'
-    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_filepath,
-        save_weights_only=True,
-        monitor='val_accuracy',
-        mode='max',
-        save_best_only=True)
-    
-    
-    print(checkpoint_filepath)
-    
-    
-    logdir = "logs/scalars/" + date_str
-    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1)
-    #, callbacks=[tensorboard_callback]
-    
-    history = model.fit(
-        train_batches,
-        epochs=30,
-        validation_data=test_batches, validation_steps=20, callbacks=[tensorboard_callback, model_checkpoint_callback]
-    )
-    
-    
-        
-        
-        
-        
-    #model = tf.keras.Sequential([
+        #model = tf.keras.Sequential([
     #    tf.keras.layers.Embedding(len(vocab), embedding_dim),
     #    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(embedding_dim)),
     #    tf.keras.layers.Dense(32, activation='relu'),
@@ -162,11 +154,60 @@ def main():
     #])
     
     
-    model.summary()
-
-
-
-
+    model = keras.Sequential([
+        layers.Embedding(len(vocab)+1, embedding_dim, mask_zero=True),
+        layers.GlobalAveragePooling1D(),
+        layers.Dense(32, activation='relu'),
+        layers.Dense(len(vocab_ret_types))
+    ])
+        
+    model.summary()    
+    
+    #####################
+    ### specify optimizer,loss,etc
+    #####################
+    
+    #model.compile(optimizer='adam',
+    #              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    #              metrics=['accuracy'])
+        
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    
+    
+    date_str = datetime.now().strftime("%Y%m%d-%H%M%S")
+    
+    #####################
+    ###specify callbacks to store checkpoints for tensorboard
+    #####################
+    
+    #checkpoint_filepath = 'logs/' + date_str
+    checkpoint_filepath = '/tmp/logs/checkpoint'
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_filepath,
+        save_weights_only=True,
+        monitor='val_accuracy',
+        mode='max',
+        save_best_only=True)
+    
+    
+    print(f'Storing tf checkpoint files to: {checkpoint_filepath}')
+    
+    
+    logdir = "/tmp/logs/scalars/" + date_str
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1)
+    #, callbacks=[tensorboard_callback]
+    print(f'Storing tensorboard files to: {logdir}')
+    
+    
+    
+    history = model.fit(
+        train_batches,
+        epochs=30,
+        validation_data=test_batches, validation_steps=20, callbacks=[tensorboard_callback, model_checkpoint_callback]
+    )
+        
 
     
 
