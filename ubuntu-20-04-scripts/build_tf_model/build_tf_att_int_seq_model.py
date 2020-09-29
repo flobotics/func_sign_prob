4c51444bc639a5cb3efe5220bc789dd09110bdae
ubu <ubu@ubu-VirtualBox>
2020-09-29 19:56:32 +0200

    val_acccuracy next

--- ubuntu-20-04-scripts/build_tf_model/build_tf_att_int_seq_model.py
+++ ubuntu-20-04-scripts/build_tf_model/build_tf_att_int_seq_model.py
@@ -228,7 +228,7 @@ def main():
         
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-                  metrics=['val_acc'])
+                  metrics=['val_accuracy'])
     
     
     date_str = datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -242,7 +242,7 @@ def main():
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         save_weights_only=True,
-        monitor='val_acc',
+        monitor='val_accuracy',
         mode='max',
         save_best_only=True)
     
