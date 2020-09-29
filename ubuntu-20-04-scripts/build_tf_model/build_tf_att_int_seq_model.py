0f70d67daa31a14734b8f4599cc5463d18b67ee8
ubu <ubu@ubu-VirtualBox>
2020-09-29 20:03:43 +0200

    val

--- ubuntu-20-04-scripts/build_tf_model/build_tf_att_int_seq_model.py
+++ ubuntu-20-04-scripts/build_tf_model/build_tf_att_int_seq_model.py
@@ -228,7 +228,7 @@ def main():
         
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-                  metrics=['val_acc'])
+                  metrics=['val_accuracy'])
     
     
     date_str = datetime.now().strftime("%Y%m%d-%H%M%S")
