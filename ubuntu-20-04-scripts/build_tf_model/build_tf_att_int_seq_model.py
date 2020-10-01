a718cbb250121a46d215ef8996e796e5420fc52c
ubu <ubu@ubu-VirtualBox>
2020-10-01 21:47:28 +0200

    freq and

--- ubuntu-20-04-scripts/build_tf_model/build_tf_att_int_seq_model.py
+++ ubuntu-20-04-scripts/build_tf_model/build_tf_att_int_seq_model.py
@@ -188,8 +188,8 @@ def main():
     ### shuffle and pad
     #train_ds_batch = train_data.shuffle(1000).padded_batch(int(len_big_int_seq))
     #test_ds_batch = test_data.shuffle(1000).padded_batch(int(len_big_int_seq))
-    train_ds_batch = train_data.shuffle(1000).padded_batch(100)
-    test_ds_batch = test_data.shuffle(1000).padded_batch(100)
+    train_ds_batch = train_data.shuffle(100).padded_batch(10)
+    test_ds_batch = test_data.shuffle(100).padded_batch(10)
     
     num_elements_in_train_ds_batch = tf.data.experimental.cardinality(train_ds_batch).numpy()
     print(f'We got {num_elements_in_train_ds_batch} in  train_ds_batch after padding')
@@ -294,7 +294,7 @@ def main():
     
     
     logdir = "/tmp/logs/scalars/" + date_str + "/"
-    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1)
     #tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
     print(f'Storing tensorboard files to: {logdir}')
     
