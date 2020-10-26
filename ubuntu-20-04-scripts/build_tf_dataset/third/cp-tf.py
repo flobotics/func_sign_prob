9d455baa70c0f4ecb962ec93c63aa550e5ed9f72
ubu <ubu@ubu-VirtualBox>
2020-10-26 17:51:57 +0100

    copy

---
+++ ubuntu-20-04-scripts/build_tf_dataset/third/cp-tf.py
@@ -0,0 +1,28 @@
+import tarfile
+import os
+import sys
+import pickle
+#import tensorflow as tf
+from datetime import datetime
+from multiprocessing import Pool
+import getopt
+from itertools import repeat
+
+
+def main():
+    files = os.listdir("/tmp/tf_record_dir/")
+    
+    for file in files:
+        print(f'file >{file}<')
+        src_file = "/home/infloflo/backup/save_dir/" + file.replace('.tfrecord', '.pickle')
+        print(f'src_file >{src_file}<')
+        dst_file = "/home/infloflo/backup/tmpstore/" + file
+        print(f'dst_file >{dst_file}<')
+        #os.rename(src_file, dst_file)
+        exit()
+    
+
+
+
+if __name__ == "__main__":
+    main()
