0cd78b2cab58c9051c3f9a2115ffb12793ff6bab
ubu <ubu@ubu-VirtualBox>
2020-11-06 18:37:02 +0100

    test

--- ubuntu-20-04-scripts/build_tf_dataset/caller_callee/train_caller_callee_model_lstm.py
+++ ubuntu-20-04-scripts/build_tf_dataset/caller_callee/train_caller_callee_model_lstm.py
@@ -280,10 +280,10 @@ def main():
 #                                     tf.keras.layers.Dense(len(return_type_dict))])
 
     model = tf.keras.Sequential([ tf.keras.layers.Embedding(len(vocabulary)+2, embedding_dim, mask_zero=True),
-                                 tf.keras.layers.LSTM(64, return_sequences=True),
-                                 tf.keras.layers.Dropout(0.2),
-                                 tf.keras.layers.GlobalAveragePooling1D(),
-                                 tf.keras.layers.Dropout(0.2),
+                                 tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+                                 tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+                                 tf.keras.layers.Dense(64, activation='relu'),
+                                 tf.keras.layers.Dropout(0.5),
                                     tf.keras.layers.Dense(len(return_type_dict))])
     
     model.summary()
