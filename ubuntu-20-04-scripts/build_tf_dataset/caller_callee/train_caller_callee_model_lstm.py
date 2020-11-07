82cffb1378d8e7f3a76e38f89f189dfb8cd25dfd
ubu <ubu@ubu-VirtualBox>
2020-11-07 18:20:09 +0100

    test

--- ubuntu-20-04-scripts/build_tf_dataset/caller_callee/train_caller_callee_model_lstm.py
+++ ubuntu-20-04-scripts/build_tf_dataset/caller_callee/train_caller_callee_model_lstm.py
@@ -279,17 +279,17 @@ def main():
 #                                     tf.keras.layers.Dropout(0.2),
 #                                     tf.keras.layers.Dense(len(return_type_dict))])
 
-#     model = tf.keras.Sequential([tf.keras.layers.Embedding(len(vocabulary)+2, embedding_dim, mask_zero=True),
-#                                  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
-#                                  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
-#                                  tf.keras.layers.Dense(64, activation='relu'),
-#                                  tf.keras.layers.Dropout(0.5),
-#                                  tf.keras.layers.Dense(len(return_type_dict))])
-
     model = tf.keras.Sequential([tf.keras.layers.Embedding(len(vocabulary)+2, embedding_dim, mask_zero=True),
-                                 tf.keras.layers.LSTM(64),
-                                 tf.keras.layers.Dense(64),
+                                 tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+                                 tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+                                 tf.keras.layers.Dense(64, activation='relu'),
+                                 tf.keras.layers.Dropout(0.5),
                                  tf.keras.layers.Dense(len(return_type_dict))])
+
+#     model = tf.keras.Sequential([tf.keras.layers.Embedding(len(vocabulary)+2, embedding_dim, mask_zero=True),
+#                                  tf.keras.layers.LSTM(64),
+#                                  tf.keras.layers.Dense(64),
+#                                  tf.keras.layers.Dense(len(return_type_dict))])
     
     model.summary()
     
