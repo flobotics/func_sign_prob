99450da97cdbfd2292d9158286be4aaf67aafb8b
ubu <ubu@ubu-VirtualBox>
2020-11-16 00:25:46 +0100

    backup

--- ubuntu-20-04-scripts/cutter_plugin/func_sign_prob_plugin.py
+++ ubuntu-20-04-scripts/cutter_plugin/func_sign_prob_plugin.py
@@ -18,13 +18,15 @@ class MyDockWidget(cutter.CutterDockWidget):
 
     def update_contents(self):
         
-        ### setup stuff
+        ### setup stuff to get gdb-style disassembly
         cutter.cmd("e asm.syntax=att")
         asm_syntax = cutter.cmd("e asm.syntax")
         cutter.cmd("e asm.arch=x86")
         asm_arch = cutter.cmd("e asm.arch")
         cutter.cmd("e asm.xrefs=false")
         asm_xrefs = cutter.cmd("e asm.xrefs")
+        cutter.cmd("e asm.bytes=false")
+        asm_bytes = cutter.cmd("e asm.bytes")
         
         
         ## find data/code references to this address with $F
@@ -55,80 +57,24 @@ class MyDockWidget(cutter.CutterDockWidget):
         file = open("/tmp/out.txt", 'w+')
         file.write("asm_syntax----------------\n")
         file.write(asm_syntax)
+        
         file.write("asm_arch------------------\n")
         file.write(asm_arch)
+        
         file.write("asm_xrefs-----------------\n")
         file.write(asm_xrefs)
+        
+        file.write("asm_bytes-----------------\n")
+        file.write(asm_bytes)
+        
         file.write("\ndisasm_caller-----------\n")
         file.write(disasm_caller)
+        
         file.write("\ndisasm_callee-----------\n")
         file.write(disasm_callee)
         file.close()
         
-        
-        
-        
-#         offset = cutter.cmd("s").strip()
-#         #self._label.setText("current offset:\n{}".format(offset))
-#         
-#         ###get disas of currently selected function
-#         ## e asm.arch=    asm.assembler=  asm.syntax=att
-#         ## e? asm
-#         disasm_callee = cutter.cmd("pdf @ " + offset).strip()
-#         
-#         ###get XREFS, then get disas of XREFs
-#         xrefs_addr_list = list()
-#         xrefs_list = list()
-#         
-#         ### axtj $F   ## $F current function
-#         ### cutter.cmdj("axtj $F")
-#         prev_word = ''
-#         for line in disasm_callee.split('\n'):
-#             #print(f'elem >{elem}<')
-#             if 'CALL XREF' in line:
-#                 xrefs_list.append(line)
-#                 for word in line.split():
-#                     print(f'word >{word}<')
-#                     if '@' in word:
-#                         xrefs_addr_list.append(prev_word)
-#                     prev_word = word
-#                 
-#         #self._label.setText("xrefs_addr_list:{}\n xrefs_list:{}".format(xrefs_addr_list, xrefs_list))
-#         
-#         ### get disas of caller function
-#         ##if there are more call-xrefs ?
-#         addr = 0
-#         for a in xrefs_addr_list:
-#             addr = a
-#         if addr != 0:
-#             disasm_caller = cutter.cmd("pdf @ " + str(addr)).strip()
-#         else:
-#             disasm_caller = ''
-#         
-#         self._label.setText("xrefs_addr_list:{}\n xrefs_list:{}\n disasm_caller:{}\n disas_callee:{}".format(xrefs_addr_list, xrefs_list, disasm_caller, disasm_callee))
-#         
-#         ## get a gdb-like disas output
-#         cutter.cmd("e asm.xrefs=false")
-#         disasm_callee = cutter.cmd("pdf @ " + offset).strip()
-#         disasm_caller = cutter.cmd("pdf @ " + str(addr)).strip()
-#         
-#         file = open("/tmp/out.txt", 'w+')
-#         file.write("asm_syntax----------------\n")
-#         file.write(asm_syntax)
-#         file.write("asm_arch------------------\n")
-#         file.write(asm_arch)
-#         file.write("asm_xrefs-----------------\n")
-#         file.write(asm_xrefs)
-#         file.write("\nxrefs_addr_list-----------\n")
-#         file.write(''.join(xrefs_addr_list))
-#         file.write("\nxrefs_list----------------\n")
-#         file.write(''.join(xrefs_list))
-#         file.write("\ndisasm_caller-------------\n")
-#         file.write(''.join(disasm_caller))
-#         file.write("\ndisasm_callee-------------\n")
-#         file.write(''.join(disasm_callee))
-#         file.write("\n--------------------------")
-#         file.close()
+       
 
 
 class MyCutterPlugin(cutter.CutterPlugin):
