d61a3ac62be91591c89c8417d088780ba09fda70
ubu <ubu@ubu-VirtualBox>
2020-11-27 01:19:22 +0100

    backup

--- ubuntu-20-04-scripts/lib/return_type_lib.py
+++ ubuntu-20-04-scripts/lib/return_type_lib.py
@@ -355,6 +355,7 @@ def get_arg_one_name_from_function_signature(function_signature):
     
     
 def get_arg_two_name_from_function_signature(function_signature):
+    print(f'function_signature >{function_signature}<')
     ### find ( which marks the function-names start
     fn_end_idx = function_signature.index('(')
     
@@ -362,7 +363,7 @@ def get_arg_two_name_from_function_signature(function_signature):
     nr_args = get_nr_of_args_from_function_signature(function_signature)
     
     if nr_args < 2:
-        print(f'not found-nr-args >{nr_args}<')
+        print(f'no second arg >{nr_args}<')
         return 'not-found'
     ##if more than one arg, filter till first comma
     
@@ -370,17 +371,21 @@ def get_arg_two_name_from_function_signature(function_signature):
     if nr_args > 2:
         
         first_comma = function_signature.index(',')
-        second_comma = function_signature.index(',')
-        arg_two = function_signature[first_comma+1:second_comma:]
+        second_comma = function_signature[first_comma+1:].index(',')
+        #print(f'first_comma >{first_comma}< second_comma >{second_comma}< >{first_comma+second_comma}')
+        fs = first_comma + second_comma + 1
+        arg_two = function_signature[first_comma+1:fs:].strip()
         
-        print(f'arg-two-nr >2 >{arg_two}<')
+        #print(f'arg-two-nr >2 >{arg_two}<')
     else:
         first_comma = function_signature.index(',')
         last_par = function_signature[::-1].index(')')
-        arg_two = function_signature[first_comma+1:last_par:]
-        print(f'arg-two-nr else >{arg_two}<')
+        #print(f'first_comma >{first_comma}<  last_par >{last_par}<')
+        lp = len(function_signature) - last_par
+        arg_two = function_signature[first_comma+1:lp:]
+        #print(f'arg-two-nr =2 >{arg_two}<')
         
-    print(f'arg_two >{arg_two}<')
+    #print(f'arg_two >{arg_two}<')
     
     return arg_two
 
