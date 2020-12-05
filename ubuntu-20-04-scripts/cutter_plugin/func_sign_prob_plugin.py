d127fc02647477841b4856deb714c39195c2a42a
ubu <ubu@ubu-VirtualBox>
2020-12-05 01:54:21 +0100

    backup

--- ubuntu-20-04-scripts/cutter_plugin/func_sign_prob_plugin.py
+++ ubuntu-20-04-scripts/cutter_plugin/func_sign_prob_plugin.py
@@ -471,28 +471,6 @@ class InferenceClass(QThread):
                 <span style=\"background-color:red;\">({arg_two_biggest_prob_percent:3.1f}%)</span> \
                 )')
             
-            
-#             self._disasTextEdit.setPlainText(f"tf return type model summary:\n \
-#                                         {ret_type_model_summary_str}\n \
-#                                         {ret_type_prediction_summary_str}\n \
-#                                         tf nr_of_args model summary:\n \
-#                                         {nr_of_args_model_summary_str}\n \
-#                                         {nr_of_args_prediction_summary_str}\n \
-#                                         tf arg_one model summary:\n \
-#                                         {arg_one_model_summary_str}\n \
-#                                         {arg_one_prediction_summary_str}\n \
-#                                         tf arg_two model summary:\n \
-#                                         {arg_two_model_summary_str}\n \
-#                                         {arg_two_prediction_summary_str}")
-#   
-#             self._funcSignLabel.setText(f'{ret_type_biggest_prob_type} \
-#                 <span style=\"background-color:red;\">({ret_type_biggest_prob_percent:3.1f}%)</span> \
-#                 {current_func_name} ( \
-#                 {arg_one_biggest_prob_type} \
-#                 <span style=\"background-color:red;\">({arg_one_biggest_prob_percent:3.1f}%)</span> , \
-#                 {arg_two_biggest_prob_type} \
-#                 <span style=\"background-color:red;\">({arg_two_biggest_prob_percent:3.1f}%)</span> \
-#                 )') 
               
             self.set_stored_radare2_e()
             return
@@ -547,34 +525,6 @@ class InferenceClass(QThread):
                 {arg_three_biggest_prob_type} \
                 <span style=\"background-color:red;\">({arg_three_biggest_prob_percent:3.1f}%)</span> \
                 )')
-            
-            
-#             self._disasTextEdit.setPlainText(f"tf return type model summary:\n \
-#                                         {ret_type_model_summary_str}\n \
-#                                         {ret_type_prediction_summary_str}\n \
-#                                         tf nr_of_args model summary:\n \
-#                                         {nr_of_args_model_summary_str}\n \
-#                                         {nr_of_args_prediction_summary_str}\n \
-#                                         tf arg_one model summary:\n \
-#                                         {arg_one_model_summary_str}\n \
-#                                         {arg_one_prediction_summary_str}\n \
-#                                         tf arg_two model summary:\n \
-#                                         {arg_two_model_summary_str}\n \
-#                                         {arg_two_prediction_summary_str}\n \
-#                                         tf arg_three model summary:\n \
-#                                         {arg_three_model_summary_str}\n \
-#                                         {arg_three_prediction_summary_str}")
-#  
-#             self._funcSignLabel.setText(f'{ret_type_biggest_prob_type} \
-#                 <span style=\"background-color:red;\">({ret_type_biggest_prob_percent:3.1f}%)</span> \
-#                 {current_func_name} ( \
-#                 {arg_one_biggest_prob_type} \
-#                 <span style=\"background-color:red;\">({arg_one_biggest_prob_percent:3.1f}%)</span> , \
-#                 {arg_two_biggest_prob_type} \
-#                 <span style=\"background-color:red;\">({arg_two_biggest_prob_percent:3.1f}%)</span> , \
-#                 {arg_three_biggest_prob_type} \
-#                 <span style=\"background-color:red;\">({arg_three_biggest_prob_percent:3.1f}%)</span> \
-#                 )') 
               
             self.set_stored_radare2_e()
             return
@@ -643,7 +593,8 @@ class FuncSignProbDockWidget(cutter.CutterDockWidget):
            
         
     def update_contents(self):
-        self._funcSignLabel.setText('Please wait ...')
+        self._funcSignLabel.setText('Running Inference, Please wait ...could take some time')
+        self._disasTextEdit.setPlainText('') 
         self._progressBar.setValue(0)
         
         self.inferenceClass.start()
