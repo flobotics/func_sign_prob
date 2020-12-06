451e07195e4beb42a7cdbcc6e64bb9f4c85a4541
ubu <ubu@ubu-VirtualBox>
2020-12-06 21:15:04 +0100

    backup

--- ubuntu-20-04-scripts/train_models/nr_of_arguments/train_nr_of_args_model_lstm.py
+++ ubuntu-20-04-scripts/train_models/nr_of_arguments/train_nr_of_args_model_lstm.py
@@ -10,97 +10,12 @@ import pickle_lib
 import common_stuff_lib
 
 
-# def parseArgs():
-#     short_opts = 'hs:w:t:r:m:v:f:b:'
-#     long_opts = ['work-dir=', 'save-dir=', 'save-file-type=', 'balanced-dataset-dir=',
-#                  'return-type-dict-file', 'max-seq-length-file=', 'vocab-file=', 'tfrecord-dir=']
-#     config = dict()
-#     
-#     config['work_dir'] = ''
-#     config['save_dir'] = ''
-#     config['save_file_type'] = ''
-#     config['return_type_dict_file'] = ''
-#     config['max_seq_length_file'] = ''
-#     config['vocabulary_file'] = ''
-#     config['tfrecord_dir'] = ''
-#     config['tensorboard_log_dir'] = ''
-#     config['checkpoint_dir'] = '' ##need to be in base-dir for projector to work
-#     config['save_model_dir'] = ''
-#     config['trained_word_embeddings_dir'] = ''
-#     config['balanced_dataset_dir'] = ''
-#     
-#  
-#     try:
-#         args, rest = getopt.getopt(sys.argv[1:], short_opts, long_opts)
-#     except getopt.GetoptError as msg:
-#         print(msg)
-#         print(f'Call with argument -h to see help')
-#         exit()
-#     
-#     for option_key, option_value in args:
-#         if option_key in ('-w', '--work-dir'):
-#             config['work_dir'] = option_value[1:]
-#         elif option_key in ('-s', '--save-dir'):
-#             config['save_dir'] = option_value[1:]
-#         elif option_key in ('-t', '--save-file-type'):
-#             config['save_file_type'] = option_value[1:]
-#         elif option_key in ('-r', '--return-type-dict-file'):
-#             config['return_type_dict_file'] = option_value[1:]
-#         elif option_key in ('-m', '--max-seq-length-file'):
-#             config['max_seq_length_file'] = option_value[1:]
-#         elif option_key in ('-v', '--vocab-file'):
-#             config['vocabulary_file'] = option_value[1:]
-#         elif option_key in ('-f', '--tfrecord-dir'):
-#             config['tfrecord_dir'] = option_value[1:]
-#         elif option_key in ('-b', '--balanced-dataset-dir'):
-#             config['balanced_dataset_dir'] = option_value[1:]
-#         elif option_key in ('-h'):
-#             print(f'<optional> -p or --pickle-dir The directory with disassemblies,etc. Default: ubuntu-20-04-pickles')
-#             print(f'<optional> -w or --work-dir   The directory where we e.g. untar,etc. Default: /tmp/work_dir/')
-#             print(f'<optional> -s or --save-dir   The directory where we save dataset.  Default: /tmp/save_dir')
-#             print(f'<optional> -b or --balanced-dataset-dir  The directory where we save the balanced dataset. Default: /tmp/save_dir/balanced/')
-#             
-#     if config['work_dir'] == '':
-#         config['work_dir'] = '/tmp/work_dir/'
-#     if config['save_dir'] == '':
-#         config['save_dir'] = '/tmp/save_dir/'
-#     if config['save_file_type'] == '':
-#         config['save_file_type'] = 'pickle'
-#     if config['return_type_dict_file'] == '':
-#         config['return_type_dict_file'] = config['save_dir'] + 'tfrecord/' + 'return_type_dict.pickle'
-#     if config['max_seq_length_file'] == '':
-#         config['max_seq_length_file'] = config['save_dir'] + 'tfrecord/' + 'max_seq_length.pickle'
-#     if config['vocabulary_file'] == '':
-#         config['vocabulary_file'] = config['save_dir'] + 'tfrecord/' + 'vocabulary_list.pickle'
-#     if config['tfrecord_dir'] == '':
-#         config['tfrecord_dir'] = config['save_dir'] + 'tfrecord/'
-#     if config['tensorboard_log_dir'] == '':
-#         config['tensorboard_log_dir'] = config['save_dir'] + 'tensorboard_logs/'
-#     if config['checkpoint_dir'] == '':
-#         config['checkpoint_dir'] = config['tensorboard_log_dir'] + 'caller_callee_checkpoint' ##need to be in base-dir for projector to work
-#     if config['save_model_dir'] == '':
-#         config['save_model_dir'] = config['tensorboard_log_dir'] +  'saved_model/'
-#     if config['trained_word_embeddings_dir'] == '':
-#         config['trained_word_embeddings_dir'] = config['tensorboard_log_dir'] +  'trained_word_embeddings/'
-#     if config['balanced_dataset_dir'] == '':
-#         config['balanced_dataset_dir'] = config['save_dir'] + 'balanced/'
-#             
-#     return config
-
 
 def check_config(config):
-#     if not os.path.isdir(config['tfrecord_dir'] + 'train/'):
-#         print(f"Directory with train tfrecord files >{config['tfrecord_dir'] + 'train/'}< does not exist")
-#         exit()
-#         
-#     if not os.path.isdir(config['tfrecord_dir'] + 'val/'):
-#         print(f"Directory with train tfrecord files >{config['tfrecord_dir'] + 'val/'}< does not exist")
-#         exit()
-#         
-#     if not os.path.isdir(config['tfrecord_dir'] + 'test/'):
-#         print(f"Directory with train tfrecord files >{config['tfrecord_dir'] + 'test/'}< does not exist")
-#         exit()
-    pass
+    if config['base_dir'] == '':
+        print(f'Please specify a base-dir (-b or --base-dir) , where all work is done. Check -h for help.')
+        print()
+        exit()
 
 
 def _parse_function(example_proto):
