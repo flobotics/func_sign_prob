0c1b3cae8d4dfce77348972450dc53ee64a29a7b
ubu <ubu@ubu-VirtualBox>
2020-12-08 00:06:30 +0100

    backup

--- ubuntu-20-04-scripts/train_models/arg_two/build_arg_two_dataset.py
+++ ubuntu-20-04-scripts/train_models/arg_two/build_arg_two_dataset.py
@@ -144,9 +144,9 @@ def proc_build(tarbz2_file, work_dir, save_dir, config):
                                         #dis2_str = dis_split(dis2_str)
                                         
                                         ##the max-seq-length blows memory (>160GB ram) with model.fit() if e.g. over 6million
-                                        if (len(dis1_str) > 100000) or (len(dis2_str) > 100000) or (len(dis1_str) < 1) or (len(dis2_str) < 1):
-                                            print(f'dis1_str >{len(dis1_str)}<')
-                                            print(f'dis2_str >{len(dis2_str)}<')
+                                        if (len(dis1_str) > (int(config['tokenized_disassembly_length'])/2)) or (len(dis2_str) > (int(config['tokenized_disassembly_length'])/2)) or (len(dis1_str) < 1) or (len(dis2_str) < 1):
+                                            print(f'tokenized_disassembly_length caller >{len(dis1_str)}<')
+                                            print(f'tokenized_disassembly_length callee >{len(dis2_str)}<')
                                             #print(f"package >{elem[2]}< bin >{elem[3]}< file >{elem[6]}< func >{elem[7]}<")
                                             #print(f"package >{elem2[2]}< bin >{elem2[3]}< file >{elem2[6]}< func >{elem2[7]}<")
                                             
