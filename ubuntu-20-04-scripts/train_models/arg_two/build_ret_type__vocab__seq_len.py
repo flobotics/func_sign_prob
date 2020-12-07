0f6ff907aa41399816d5fe77aa1fe83e021db185
ubu <ubu@ubu-VirtualBox>
2020-12-07 19:33:32 +0100

    backup

---
+++ ubuntu-20-04-scripts/train_models/arg_two/build_ret_type__vocab__seq_len.py
@@ -0,0 +1,150 @@
+import tarfile
+import os
+import sys
+import pickle
+#import tensorflow as tf
+from datetime import datetime
+from multiprocessing import Pool
+import getopt
+from itertools import repeat
+import psutil
+
+sys.path.append('../../lib/')
+import return_type_lib
+import common_stuff_lib
+import tarbz2_lib
+import pickle_lib
+import disassembly_lib
+#import tfrecord_lib
+
+
+
+
+def check_config(config):
+    if config['base_dir'] == '':
+        print(f'Please specify a base-dir (-b or --base-dir) , where all work is done. Check -h for help.')
+        print()
+        exit()
+
+
+def proc_build(file, config):
+    
+    ret_set = set()
+    vocab = set()
+    seq_length = 0
+    
+    print(f'File >{file}<')
+    
+    cont = pickle_lib.get_pickle_file_content(file)
+    for item in cont:
+        #print(f'item-1 >{item[1]}<')
+        ## build ret-type-dict
+        ret_set.add(item[1])
+        
+        ##build max-seq-length
+        if len(item[0]) > seq_length:
+            if len(item[0]) > 100000:
+                print(f'len-bigger 100.000')
+            seq_length = len(item[0])
+            
+        ## build vocabulary
+        for word in item[0].split():
+            vocab.add(word)
+            
+    return (ret_set, vocab, seq_length)
+
+
+
+
+def main():
+    config = common_stuff_lib.parseArgs()
+    check_config(config)
+    
+    nr_of_cpus = psutil.cpu_count(logical=True)
+    print(f'We got >{nr_of_cpus}< CPUs for threading')
+    print()
+    
+    ## build return type dict-file and max-seq-length-file and vocabulary
+    pickle_files = common_stuff_lib.get_all_filenames_of_type(config['save_dir'], '.pickle')
+    print(f'pickle-files we use to build >{pickle_files}<')
+    
+    print(f'Building return-type dict, vocabulary and max-squenece-length')
+    
+    p = Pool(nr_of_cpus)
+    
+    pickle_files = [config['save_dir'] + "/" + f for f in pickle_files]
+    
+    star_list = zip(pickle_files, repeat(config))
+    
+    all_ret_types = p.starmap(proc_build, star_list)
+    p.close()
+    p.join()
+    
+    ret_set = set()
+    vocab = set()
+    seq_length = 0
+    
+    ##put all stuff together
+    for ret_set1, vocab1, seq_length1 in all_ret_types:
+        ret_set.update(ret_set1)
+        vocab.update(vocab1)
+        if seq_length1 > seq_length:
+            seq_length = seq_length1
+    
+    
+    
+    
+    
+#     ret_set = set()
+#     vocab = set()
+#     seq_length = 0
+#     counter = 1
+#     pickle_count = len(pickle_files)
+#     
+#     for file in pickle_files:
+#         print(f'File >{file}< >{counter}/{pickle_count}<', end='\r')
+#         counter += 1
+#         cont = pickle_lib.get_pickle_file_content(config['save_dir'] + file)
+#         for item in cont:
+#             #print(f'item-1 >{item[1]}<')
+#             ## build ret-type-dict
+#             ret_set.add(item[1])
+#             
+#             ##build max-seq-length
+#             if len(item[0]) > seq_length:
+#                 seq_length = len(item[0])
+#                 
+#             ## build vocabulary
+#             for word in item[0].split():
+#                 vocab.add(word)
+        
+    print(f"Build return-type dict from set and save it to >{config['return_type_dict_file']}<")        
+    ## build ret-type-dict and save
+    ret_type_dict = dict()
+    counter = 0
+    for elem in ret_set:
+        ret_type_dict[elem] = counter
+        counter += 1
+    
+    print(f"ret-type-dict :")
+    for key in ret_type_dict:
+        print(f"key >{key}<  value >{ret_type_dict[key]}<")
+        
+    pickle_lib.save_to_pickle_file(ret_type_dict, config['return_type_dict_file'])
+        
+    print(f"Saving vocabulary to >{config['vocabulary_file']}<")
+    ## build vocabulary list from set and save
+    vocab_list = list(vocab)
+    pickle_lib.save_to_pickle_file(vocab_list, config['vocabulary_file'])
+    
+    ## save max-seq-length
+    print(f"Saving max-sequence-length to >{config['max_seq_length_file']}<")
+    pickle_lib.save_to_pickle_file(seq_length, config['max_seq_length_file'])
+    
+    
+    print("Done. Run build_balanced_dataset.py next")
+    
+    
+
+if __name__ == "__main__":
+    main()
---
+++ ubuntu-20-04-scripts/train_models/arg_two/transform_ret_type_to_int.py
@@ -0,0 +1,89 @@
+import tarfile
+import os
+import sys
+import pickle
+import tensorflow as tf
+from datetime import datetime
+from multiprocessing import Pool
+import getopt
+from itertools import repeat
+import psutil
+
+
+sys.path.append('../../lib/')
+import return_type_lib
+import common_stuff_lib
+import tarbz2_lib
+import pickle_lib
+import disassembly_lib
+import tfrecord_lib
+
+
+
+
+def check_config(config):
+    if not os.path.isdir(config['save_dir']):
+        print(f"Directory >{config['save_dir']}< does not exist")
+        exit()
+        
+    if not os.path.isdir(config['tfrecord_save_dir']):
+        print(f"Directory >{config['tfrecord_save_dir']}< does not exist. Create it.")
+        exit()
+        
+ 
+def proc_build(file, ret_type_dict, config):
+    trans_ds = list()
+    
+    print(f'Transform File >{file}<')
+    
+    cont = pickle_lib.get_pickle_file_content(file)
+    for item in cont:
+        print(f"item >{item[0]}<  item-1 >{item[1]}< >{ret_type_dict[item[1]]}<")
+        trans_ds.append( (item[0], ret_type_dict[item[1]]) )
+        
+    tfrecord_lib.save_caller_callee_to_tfrecord(trans_ds, config['tfrecord_save_dir'] + os.path.basename(file).replace('.pickle', '.tfrecord'))
+
+
+def check_config(config):
+    if config['base_dir'] == '':
+        print(f'Please specify a base-dir (-b or --base-dir) , where all work is done. Check -h for help.')
+        print()
+        exit()
+        
+    print(f'config >{config}<')
+    print()
+         
+
+
+def main():
+    config = common_stuff_lib.parseArgs()
+    check_config(config)
+    
+    nr_of_cpus = psutil.cpu_count(logical=True)
+    print(f'We got >{nr_of_cpus}< CPUs for threading')
+    print()
+
+    ##load ret-type dict
+    ret_type_dict = pickle_lib.get_pickle_file_content(config['return_type_dict_file'])
+    print(f"ret-type-dict >{ret_type_dict}<")
+    
+    pickle_files = common_stuff_lib.get_all_filenames_of_type(config['balanced_dataset_dir'], '.pickle')
+    
+    ### transform dataset ret-types to ints
+    print(f"Transform return-type to int and save to >{config['tfrecord_save_dir']}<")
+    p = Pool(nr_of_cpus)
+    
+    pickle_files = [config['balanced_dataset_dir'] + "/" + f for f in pickle_files]
+    
+    star_list = zip(pickle_files, repeat(ret_type_dict), repeat(config))
+    
+    all_ret_types = p.starmap(proc_build, star_list)
+    p.close()
+    p.join()
+    
+       
+
+    print("Done. Run train_arg_two_model_lstm.py next")
+
+if __name__ == "__main__":
+    main()
