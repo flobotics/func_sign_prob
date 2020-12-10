26f66e78fc5a8709f85b11461cac54772aae85a4
ubu <ubu@ubu-VirtualBox>
2020-12-10 20:39:07 +0100

    backup

--- ubuntu-20-04-scripts/train_models/return_type/build_return_type_dataset.py
+++ ubuntu-20-04-scripts/train_models/return_type/build_return_type_dataset.py
@@ -1,3 +1,5 @@
+from multiprocessing import set_start_method
+set_start_method("spawn")
 import tarfile
 import os
 import sys
@@ -68,141 +70,141 @@ def tf_serialize_example(f0,f1):
 
  
 def proc_build(tarbz2_file, work_dir, save_dir, config):
-    
-    tarbz2_lib.untar_file_to_path(tarbz2_file, work_dir)
-    #untar_one_pickle_file(tarbz2_file, work_dir)
-     
-    pickle_file = work_dir + os.path.basename(tarbz2_file).replace('.tar.bz2', '')
-    pickle_file_content = pickle_lib.get_pickle_file_content(pickle_file)   
-    #pickle_file_content = get_pickle_file_content(work_dir + os.path.basename(pickle_file).replace('.tar.bz2', ''))
-    
-    binaries = set()
-    functions = set()
-    for elem in pickle_file_content:
-        binaries.add(elem[7])
-        functions.add(elem[2])
-    
-    print(f'binaries >{binaries}<')
-    
-    counter = 0
-    dataset_list = list()
-    
-    ## 1. get one binary
-    ## 2. get one function of this binary
-    ## 3. get disassembly of this function
-    ## 4. check if this disassembly calls another function
-    ## 4.1 filter @plt
-    ## 5. if yes: get disassembly of caller function
-    ## 6. save caller, callee, func_signature
-    ## 7. check again, if it calls another function
-    ## 8. if yes: get disassembly of caller function
-    ## 9. save caller, calle, func_signature
-    ##10. get disassembly of next function of this binary
-    ##11. check if ....
-    for bin in binaries:
-        for func in functions:
-            ## search for bin and func
-            for elem in pickle_file_content:
-                ### if we found bin and func
-                if elem[7] == bin and elem[2] == func:
-                    ## get att disassembly
-                    att_dis = elem[4]
-                    #print(f'att-dis >{att_dis}<')
-                    ## check every line if there is a call
-                    for item in att_dis:
-                        ## find call in disas
-                        if disassembly_lib.find_call_in_disassembly_line(item):
-                            ## if found, get callee name
-                            callee_name = disassembly_lib.get_callee_name_from_disassembly_line(item)
-                                                                   
-                            #print(f'callee_name >{callee_name}<')
-                            
-                            ## search for same bin, but callee func
-                            for elem2 in pickle_file_content:
-                                ### if we found it, get return type and disassembly
-                                if elem2[7] == bin and elem2[2] == callee_name:
-                                    
-                                    if (len(elem2[4]) > (int(config['tokenized_disassembly_length'])/2)) or (len(att_dis) > (int(config['tokenized_disassembly_length'])/2)) or (len(elem2[4]) < 1) or (len(att_dis) < 1):
-                                        continue
-                                    
-                                    return_type_func_sign = return_type_lib.get_return_type_from_function_signature(elem2[0])
-                                    return_type = return_type_lib.get_return_type_from_gdb_ptype(elem2[1])
-                                    
-                                    ###for debugging, what string is still unknown ?? should show nothing
-                                    if return_type == 'unknown':
-                                        print(f'string_before_func_name: {return_type_func_sign}')
-                                     
-                                    if return_type == 'unknown':
-                                        #print('unknown found')
-                                        #breaker = True
-                                        #break
-                                        pass
-                                    elif return_type == 'delete':
-                                        #print('delete found')
-                                        ### no return type found, so delete this item
-                                        pass
-                                    elif return_type == 'process_further':
-                                        print(f'ERRROOOORRRR---------------')
-                                    else:
-                                        tmp_att_dis = att_dis
-                                        #print(f'len att-dis 1 >{len(tmp_att_dis)}<')
-                                        tmp_att_dis = disassembly_lib.clean_att_disassembly_from_comment(tmp_att_dis)
-                                        callee_dis = disassembly_lib.clean_att_disassembly_from_comment(elem2[4])
-                                        #print(f'len att-dis 1 >{len(tmp_att_dis)}<')
-                                        #print(f'att-dis >{tmp_att_dis}<')
-                                        
-                                        dis1_str = ' '.join(tmp_att_dis)
-                                        #dis2_str = ' '.join(elem2[4])
-                                        dis2_str = ' '.join(callee_dis)
-                                        
-                                        dis1_str = disassembly_lib.split_disassembly(dis1_str)
-                                        dis2_str = disassembly_lib.split_disassembly(dis2_str)
-                                        #dis1_str = dis_split(dis1_str)
-                                        #dis2_str = dis_split(dis2_str)
-                                        
-                                        ##the max-seq-length blows memory (>160GB ram) with model.fit() if e.g. over 6million
-                                        if (len(dis1_str) > (int(config['tokenized_disassembly_length'])/2)) or (len(dis2_str) > (int(config['tokenized_disassembly_length'])/2)) or (len(dis1_str) < 1) or (len(dis2_str) < 1):
-                                            print(f'tokenized_disassembly_length caller >{len(dis1_str)}<')
-                                            print(f'tokenized_disassembly_length callee >{len(dis2_str)}<')
-                                            #print(f"package >{elem[2]}< bin >{elem[3]}< file >{elem[6]}< func >{elem[7]}<")
-                                            #print(f"package >{elem2[2]}< bin >{elem2[3]}< file >{elem2[6]}< func >{elem2[7]}<")
-                                            
-                                        else:
-                                            dis_str = dis1_str + dis2_str
-                                                
-                                            #print(f'dis_str >{dis_str}<')
-                                        
-                                            dataset_list.append((dis_str, return_type))
-                                            counter += 1
-                                            
-                                        break
-                  
-           
-    if dataset_list:
-        if config['save_file_type'] == 'pickle':     
-            ret_file = open(config['save_dir'] + os.path.basename(pickle_file).replace('.tar.bz2', ''), 'wb+')
-            pickle_list = pickle.dump(dataset_list, ret_file)
-            ret_file.close()
-        else:
-            ## save as tfrecord
-            dis_list = list()
-            ret_list = list()
-            
-            for item in dataset_list:
-                dis_list.append(item[0])
-                ret_list.append(item[1])
-                
-            raw_dataset = tf.data.Dataset.from_tensor_slices( (dis_list, ret_list ))
+    with get_context("spawn").Pool() as pool:
+        tarbz2_lib.untar_file_to_path(tarbz2_file, work_dir)
+        #untar_one_pickle_file(tarbz2_file, work_dir)
+         
+        pickle_file = work_dir + os.path.basename(tarbz2_file).replace('.tar.bz2', '')
+        pickle_file_content = pickle_lib.get_pickle_file_content(pickle_file)   
+        #pickle_file_content = get_pickle_file_content(work_dir + os.path.basename(pickle_file).replace('.tar.bz2', ''))
         
-            serialized_features_dataset = raw_dataset.map(tf_serialize_example)
+        binaries = set()
+        functions = set()
+        for elem in pickle_file_content:
+            binaries.add(elem[7])
+            functions.add(elem[2])
+        
+        print(f'binaries >{binaries}<')
+        
+        counter = 0
+        dataset_list = list()
+        
+        ## 1. get one binary
+        ## 2. get one function of this binary
+        ## 3. get disassembly of this function
+        ## 4. check if this disassembly calls another function
+        ## 4.1 filter @plt
+        ## 5. if yes: get disassembly of caller function
+        ## 6. save caller, callee, func_signature
+        ## 7. check again, if it calls another function
+        ## 8. if yes: get disassembly of caller function
+        ## 9. save caller, calle, func_signature
+        ##10. get disassembly of next function of this binary
+        ##11. check if ....
+        for bin in binaries:
+            for func in functions:
+                ## search for bin and func
+                for elem in pickle_file_content:
+                    ### if we found bin and func
+                    if elem[7] == bin and elem[2] == func:
+                        ## get att disassembly
+                        att_dis = elem[4]
+                        #print(f'att-dis >{att_dis}<')
+                        ## check every line if there is a call
+                        for item in att_dis:
+                            ## find call in disas
+                            if disassembly_lib.find_call_in_disassembly_line(item):
+                                ## if found, get callee name
+                                callee_name = disassembly_lib.get_callee_name_from_disassembly_line(item)
+                                                                       
+                                #print(f'callee_name >{callee_name}<')
+                                
+                                ## search for same bin, but callee func
+                                for elem2 in pickle_file_content:
+                                    ### if we found it, get return type and disassembly
+                                    if elem2[7] == bin and elem2[2] == callee_name:
+                                        
+                                        if (len(elem2[4]) > (int(config['tokenized_disassembly_length'])/2)) or (len(att_dis) > (int(config['tokenized_disassembly_length'])/2)) or (len(elem2[4]) < 1) or (len(att_dis) < 1):
+                                            continue
+                                        
+                                        return_type_func_sign = return_type_lib.get_return_type_from_function_signature(elem2[0])
+                                        return_type = return_type_lib.get_return_type_from_gdb_ptype(elem2[1])
+                                        
+                                        ###for debugging, what string is still unknown ?? should show nothing
+                                        if return_type == 'unknown':
+                                            print(f'string_before_func_name: {return_type_func_sign}')
+                                         
+                                        if return_type == 'unknown':
+                                            #print('unknown found')
+                                            #breaker = True
+                                            #break
+                                            pass
+                                        elif return_type == 'delete':
+                                            #print('delete found')
+                                            ### no return type found, so delete this item
+                                            pass
+                                        elif return_type == 'process_further':
+                                            print(f'ERRROOOORRRR---------------')
+                                        else:
+                                            tmp_att_dis = att_dis
+                                            #print(f'len att-dis 1 >{len(tmp_att_dis)}<')
+                                            tmp_att_dis = disassembly_lib.clean_att_disassembly_from_comment(tmp_att_dis)
+                                            callee_dis = disassembly_lib.clean_att_disassembly_from_comment(elem2[4])
+                                            #print(f'len att-dis 1 >{len(tmp_att_dis)}<')
+                                            #print(f'att-dis >{tmp_att_dis}<')
+                                            
+                                            dis1_str = ' '.join(tmp_att_dis)
+                                            #dis2_str = ' '.join(elem2[4])
+                                            dis2_str = ' '.join(callee_dis)
+                                            
+                                            dis1_str = disassembly_lib.split_disassembly(dis1_str)
+                                            dis2_str = disassembly_lib.split_disassembly(dis2_str)
+                                            #dis1_str = dis_split(dis1_str)
+                                            #dis2_str = dis_split(dis2_str)
+                                            
+                                            ##the max-seq-length blows memory (>160GB ram) with model.fit() if e.g. over 6million
+                                            if (len(dis1_str) > (int(config['tokenized_disassembly_length'])/2)) or (len(dis2_str) > (int(config['tokenized_disassembly_length'])/2)) or (len(dis1_str) < 1) or (len(dis2_str) < 1):
+                                                print(f'tokenized_disassembly_length caller >{len(dis1_str)}<')
+                                                print(f'tokenized_disassembly_length callee >{len(dis2_str)}<')
+                                                #print(f"package >{elem[2]}< bin >{elem[3]}< file >{elem[6]}< func >{elem[7]}<")
+                                                #print(f"package >{elem2[2]}< bin >{elem2[3]}< file >{elem2[6]}< func >{elem2[7]}<")
+                                                
+                                            else:
+                                                dis_str = dis1_str + dis2_str
+                                                    
+                                                #print(f'dis_str >{dis_str}<')
+                                            
+                                                dataset_list.append((dis_str, return_type))
+                                                counter += 1
+                                                
+                                            break
+                      
+               
+        if dataset_list:
+            if config['save_file_type'] == 'pickle':     
+                ret_file = open(config['save_dir'] + os.path.basename(pickle_file).replace('.tar.bz2', ''), 'wb+')
+                pickle_list = pickle.dump(dataset_list, ret_file)
+                ret_file.close()
+            else:
+                ## save as tfrecord
+                dis_list = list()
+                ret_list = list()
+                
+                for item in dataset_list:
+                    dis_list.append(item[0])
+                    ret_list.append(item[1])
+                    
+                raw_dataset = tf.data.Dataset.from_tensor_slices( (dis_list, ret_list ))
             
-            filename = config['save_dir']  + os.path.basename(tarbz2_file).replace('.pickle.tar.bz2','') + '.tfrecord'
-            writer = tf.data.experimental.TFRecordWriter(filename)
-            writer.write(serialized_features_dataset)
-    
-    
-                  
-    return counter
+                serialized_features_dataset = raw_dataset.map(tf_serialize_example)
+                
+                filename = config['save_dir']  + os.path.basename(tarbz2_file).replace('.pickle.tar.bz2','') + '.tfrecord'
+                writer = tf.data.experimental.TFRecordWriter(filename)
+                writer.write(serialized_features_dataset)
+        
+        
+                      
+        return counter
     
     
 def check_config(config):
@@ -271,7 +273,7 @@ def copy_files_to_build_dataset(config):
     print(f'Copied >{nr_files}< files')
     print()
 
-    
+
       
 def main():
     config = common_stuff_lib.parseArgs()
@@ -292,8 +294,8 @@ def main():
     
     ### build
     p = Pool(nr_of_cpus)
-    
-    
+     
+     
     pickle_files = [config["pickle_dir"] + "/" + f for f in pickle_files]
     star_list = zip(pickle_files, repeat(config['work_dir']), repeat(config['save_dir']), repeat(config))
     all_ret_types = p.starmap(proc_build, star_list)
